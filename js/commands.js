39deadc5089185e9e493c6ecbcd4f57971f529b4
XANDERCS <cartman4292@gmail.com>
2023-12-13 20:49:38 +0200

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,59 @@
+var youtube = "https://www.youtube.com/channel/UCIpgQvZL7pWEnrMUQbT57zw";
+var video = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+var twitter = "https://twitter.com/ducu_alexandru";
+var password = "xander";
+var email = 'mailto:cartman4292@gmail.com';
+
+whois = [
+  "<br>",
+  "Salut, eu sunt xaNder!👋",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: I'm nobody.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/xandercs' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/xandercs' + '</a>',
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Cine e xaNder?',
+  '<span class="command">whoami</span>         Cine esti tu?',
+  '<span class="command">video</span>          Vezi clipul video',
+  '<span class="command">social</span>         Afisati retelele sociale',
+  '<span class="command">secret</span>         Gaseste parola',
+  '<span class="command">history</span>        Vezi istoricul comenzilor',
+  '<span class="command">help</span>           Evident, stii deja ce face asta',
+  '<span class="command">email</span>          Nu imi trimite email',
+  '<span class="command">clear</span>          Sterge terminalul',
+  '<span class="command">banner</span>         Afisati Bannerul',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Ducu Alexandru - XANDERCS [Terminal]</span>',
+  " █████ █████   █████████   ██████   █████ ██████████   ██████████ ███████████     █████████   █████████ ",
+  "░░███ ░░███   ███░░░░░███ ░░██████ ░░███ ░░███░░░░███ ░░███░░░░░█░░███░░░░░███   ███░░░░░███ ███░░░░░███",
+  " ░░███ ███   ░███    ░███  ░███░███ ░███  ░███   ░░███ ░███  █ ░  ░███    ░███  ███     ░░░ ░███    ░░░ ",
+  "  ░░█████    ░███████████  ░███░░███░███  ░███    ░███ ░██████    ░██████████  ░███         ░░█████████ ",
+  "   ███░███   ░███░░░░░███  ░███ ░░██████  ░███    ░███ ░███░░█    ░███░░░░░███ ░███          ░░░░░░░░███",
+  "  ███ ░░███  ░███    ░███  ░███  ░░█████  ░███    ███  ░███ ░   █ ░███    ░███ ░░███     ███ ███    ░███",
+  " █████ █████ █████   █████ █████  ░░█████ ██████████   ██████████ █████   █████ ░░█████████ ░░█████████ ",
+  "░░░░░ ░░░░░ ░░░░░   ░░░░░ ░░░░░    ░░░░░ ░░░░░░░░░░   ░░░░░░░░░░ ░░░░░   ░░░░░   ░░░░░░░░░   ░░░░░░░░░  ",
+  '<span class="color2">Bine ai venit.</span>',
+  "<span class=\"color2\">Pentru a vedea comenzile disponibile, tasteaza</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
