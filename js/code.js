bc082c30335173fe553baee7347165603a39fa90
XANDER <cartman4292@gmail.com>
2024-09-21 22:03:12 +0300

    Add files via upload

---
+++ js/code.js
@@ -0,0 +1,2 @@
+var dayDiffCase = Math.floor((new Date(Date()).getTime() - new Date("02/09/2023").getTime()) / (1000 * 3600 * 24), 0); /* calc gap between last case */
+var dayDiffOp = Math.floor((new Date(Date()).getTime() - new Date("09/21/2021").getTime()) / (1000 * 3600 * 24), 0); /* calc gap between last operation */
